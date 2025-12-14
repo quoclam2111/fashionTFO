27fa9663b1428414c5b7bd73b7afa0155f3b3e75
quocan04 <ant962111@gmail.com>
2025-12-14 23:57:46 +0700

    Them payment vao repo

---
+++ src/main/java/repository/payment/OrderPaymentRepository.java
@@ -0,0 +1,11 @@
+package repository.payment;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import pay.entity.Order;
+
+public interface OrderPaymentRepository {
+    void save(Order order);
+    Optional<Order> findById(UUID id);
+}
